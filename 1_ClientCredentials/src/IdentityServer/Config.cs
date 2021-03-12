48b93ba693a43ec6186f85c4439528b51262625b
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-12 16:04:32 +0200

    Define the client

--- 1_ClientCredentials/src/IdentityServer/Config.cs
+++ 1_ClientCredentials/src/IdentityServer/Config.cs
@@ -14,5 +14,26 @@ namespace IdentityServer
         {
             new ApiScope("api1", "My API")
         };
+
+        public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
     }
 }
