8de909ac8bd40ac02380c67fb900e0089b054054
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-17 15:09:31 +0200

    Client
    Request token.

--- 1_ClientCredentials/src/Client/Program.cs
+++ 1_ClientCredentials/src/Client/Program.cs
@@ -18,6 +18,22 @@ namespace Client
                 Console.WriteLine(disco.Error);
                 return;
             }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.IsError);
+                return;
+            }
         }
     }
 }
