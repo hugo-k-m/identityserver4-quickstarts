617fb2aeb21765ddea96a8e975f76641233f4dc5
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-18 16:29:54 +0200

    Calling the API

--- 1_ClientCredentials/src/Client/Program.cs
+++ 1_ClientCredentials/src/Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
 namespace Client
 {
@@ -36,6 +37,22 @@ namespace Client
             }
 
             Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
