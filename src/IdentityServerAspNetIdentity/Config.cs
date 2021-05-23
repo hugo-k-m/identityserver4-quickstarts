acdccd040c17f7e94c3625e62d3cd4695d8c661a
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-05-23 11:10:13 +0200

    feat(ids4): removed old ids4 project and client configs to new project

--- src/IdentityServerAspNetIdentity/Config.cs
+++ src/IdentityServerAspNetIdentity/Config.cs
@@ -10,49 +10,66 @@ namespace IdentityServerAspNetIdentity
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
 
         public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
+        new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
 
         public static IEnumerable<Client> Clients =>
-            new Client[]
+        new List<Client>
+        {
+            // machine to machine client
+            new Client
             {
-                // m2m client credentials flow client
-                new Client
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
+                    new Secret("secret".Sha256())
                 },
 
-                // interactive client using code flow + pkce
-                new Client
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+
+            // interactive ASP.NET Core MVC client
+            new Client
+            {
+                ClientId = "mvc",
+
+                ClientSecrets =
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    new Secret("secret".Sha256())
                 },
-            };
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<String>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
+            }
+        };
     }
 }
